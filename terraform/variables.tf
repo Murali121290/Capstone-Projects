16b1ae7bea83f34a3b3a6e032826b097b1a5339d
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-09-24 15:31:42 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,24 @@
-variable "aws_region" { default = "us-east-1" }
-variable "ami_id" { default = "ami-0a91cd140a1fc148a" } # Ubuntu 20.04 (change if needed)
-variable "instance_type" { default = "t3.medium" }
-variable "key_name" { default = "all-in-one-key" }
-variable "public_key_path" {
-  default = "~/.ssh/cloudshell-key.pub"
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  default     = "us-east-1"
 }
-variable "my_ip_cidr" { default = "0.0.0.0/0" }
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.medium"
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  default     = "all-in-one-key"
+}
+
+variable "public_key_path" {
+  description = "Path to your SSH public key"
+  default     = "~/.ssh/cloudshell-key.pub"
+}
+
+variable "my_ip_cidr" {
+  description = "Your IP/CIDR to allow SSH access"
+  default     = "0.0.0.0/0"
+}
