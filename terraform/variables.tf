ec4a6a3d813383ba7d8bbccb82f4bb6f8ef905bf
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-09-24 15:36:34 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,29 @@
 variable "aws_region" {
-  description = "AWS region to deploy into"
+  description = "AWS region to deploy resources"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type        = string
   default     = "t3.medium"
 }
 
 variable "key_name" {
-  description = "Key pair name for SSH access"
+  description = "Name to assign to the AWS Key Pair"
+  type        = string
   default     = "all-in-one-key"
 }
 
 variable "public_key_path" {
-  description = "Path to your SSH public key"
+  description = "Path to your SSH public key (must exist before apply)"
+  type        = string
   default     = "~/.ssh/cloudshell-key.pub"
 }
 
 variable "my_ip_cidr" {
-  description = "Your IP/CIDR to allow SSH access"
+  description = "CIDR block allowed for SSH access (e.g., your IP/32)"
+  type        = string
   default     = "0.0.0.0/0"
 }
