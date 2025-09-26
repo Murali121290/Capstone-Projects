fcd254faa5a8450e70c72e584b79698ccd87feb0
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-09-26 10:12:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,5 @@
 variable "key_name" {
-  description = "Name of the EC2 key pair in AWS (not the .pem file)."
+  description = "The name of the EC2 key pair in AWS (not the .pem file)."
   type        = string
-  default     = "murali26jul2025"
-}
-
-variable "private_key_path" {
-  description = "Local path to the private key (.pem) used for SSH."
-  type        = string
-  default     = "/home/ubuntu/.ssh/murali26jul2025.pem"
+  default     = "murali26jul2025" # replace with your actual key pair name
 }
