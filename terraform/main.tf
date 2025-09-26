6034db069e22b8694c878d6d4a6b39a5366e1e57
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-09-26 10:02:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,52 @@ data "aws_vpc" "default" {
 
 # Security Group
 resource "aws_security_group" "ec2_sg" {
-  ...
+  name        = "dev_server_sg"
+  description = "Allow SSH, HTTP, Jenkins, and SonarQube"
+  vpc_id      = data.aws_vpc.default.id
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Jenkins
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SonarQube
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dev-server-sg"
+  }
 }
 
 # EC2 instance
