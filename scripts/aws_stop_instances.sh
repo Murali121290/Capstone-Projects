b23d6a52fe8f14f731874eee214968a5b17c9cef
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-09-26 15:01:07 +0530

    Delete scripts directory

--- scripts/aws_stop_instances.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# usage: ./aws_stop_instances.sh <instance-id>
-if [ -z "$1" ]; then
-  echo "Usage: $0 <instance-id>"
-  exit 1
-fi
-aws ec2 stop-instances --instance-ids $1 --region ${AWS_REGION:-us-east-1}
