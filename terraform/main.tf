a1c384f9b405c2127aa63c87155e99c06fd85f5c
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-09-26 10:11:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# VPC lookup
+# Lookup default VPC
 data "aws_vpc" "default" {
   default = true
 }
@@ -69,53 +69,47 @@ resource "aws_security_group" "ec2_sg" {
 
 # EC2 instance
 resource "aws_instance" "dev_server" {
-  ami           = "ami-08c40ec9ead489470"
+  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2023 us-east-1
   instance_type = "t3.medium"
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
-user_data = <<-EOF
-  #!/bin/bash
-  set -xe
+  user_data = <<-EOF
+    #!/bin/bash
+    set -xe
 
-  # Update system
-  dnf update -y
+    # Update system
+    dnf update -y
 
-  # Install basic tools
-  dnf install -y docker git curl wget unzip java-11-openjdk
+    # Install basic tools
+    dnf install -y docker git curl wget unzip java-11-openjdk
 
-  # Enable and start Docker
-  systemctl enable docker
-  systemctl start docker
-  usermod -aG docker ec2-user
+    # Enable and start Docker
+    systemctl enable docker
+    systemctl start docker
+    usermod -aG docker ec2-user
 
-  # Install Jenkins
-  rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-  curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
-  dnf install -y jenkins
-  systemctl enable jenkins
-  systemctl start jenkins
+    # Install Jenkins
+    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
+    dnf install -y jenkins
+    systemctl enable jenkins
+    systemctl start jenkins
 
-  # Run SonarQube (Docker)
-  docker run -d --name sonarqube -p 9000:9000 sonarqube:lts-community
+    # Run SonarQube (Docker)
+    docker run -d --name sonarqube -p 9000:9000 sonarqube:lts-community
 
-  # Install Minikube
-  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-  install minikube-linux-amd64 /usr/local/bin/minikube
+    # Install Minikube
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    install minikube-linux-amd64 /usr/local/bin/minikube
 
-  # Install kubectl
-  curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-EOF
-
+    # Install kubectl
+    curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  EOF
 
   tags = {
     Name        = "Dev-Server-Jenkins-SonarQube-Minikube"
     Environment = "Dev"
   }
-}
-
-# Output
-output "public_ip" {
-  value = aws_instance.dev_server.public_ip
 }
