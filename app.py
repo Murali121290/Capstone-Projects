20bf48505fdaac9a24c81023f046e617e3166ffc
Murali Balu <156786504+Murali121290@users.noreply.github.com>
2025-10-06 19:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,6 @@ app = Flask(__name__)
 def home():
     return "✅ Welcome to the Flask App!"
 
-@app.route('/')
-def home():
-    return "✅ Welcome to the Flask App!"
-
-@app.route('/')
-def home():
-    return "✅ Welcome to the Flask App!"
-
-@app.route('/')
-def home():
-    return "✅ Welcome to the Flask App!"
-
 # Run the app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
